139ab7e1aa60a31b2d35305683563ab65f8abe6e
daler <dalerr@niddk.nih.gov>
2012-09-06 13:13:18 -0400

    pep8

--- scurgen/hilbert.py
+++ scurgen/hilbert.py
@@ -5,42 +5,40 @@ import os
 import matplotlib
 import subprocess
 from pybedtools import genome_registry
-    
+import logging
 def rot(n, x, y, rx, ry):
     if (ry == 0):
         if (rx == 1):
-            x = n-1 - x
-            y = n-1 - y
+            x = n - 1 - x
+            y = n - 1 - y
         # Swap x and y
         x, y = y, x
     return (x, y)
-        
 def d2xy(n, d):
     t = d
     x = y = 0
-    
+
     s = 1
     while s < n:
-        rx = 1 & (t/2)
+        rx = 1 & (t / 2)
         ry = 1 & (t ^ rx)
         (x, y) = rot(s, x, y, rx, ry)
         x += (s * rx)
         y += (s * ry)
         t = t / 4
         s *= 2
-    return (x,y)
+    return (x, y)
 
 
 class HilbertMatrix(object):
-    
     def __init__(self, file, genome, chrom, matrix_dim, incr_column=None):
         self.file = file
         self.genome = genome
         self.chrom = chrom
-        
+
         # grab the dict of chrom lengths for this genome
         self.chromdict = pbt.chromsizes(self.genome)
-        
+
         if self.chrom != "genome":
             # grab the length of the requested genome
             self.chrom_length = self.chromdict[self.chrom][1]
@@ -53,7 +51,7 @@ class HilbertMatrix(object):
                 self.chrom_offsets[chrom] = curr_offset
                 self.chrom_length += self.chromdict[chrom][1]
                 curr_offset += self.chromdict[chrom][1]
-    
+
         self.m_dim = matrix_dim
         self.cells = self.m_dim * self.m_dim
         self.norm_factor = int(self.chrom_length / self.cells)
@@ -74,7 +72,7 @@ class HilbertMatrix(object):
         x = coords[0]
         y = coords[1]
         self.matrix[x][y] += increment
-        
+
     def _get_intervals(self):
         if not self.file.endswith('.bam'):
             return pbt.BedTool(self.file)
@@ -84,7 +82,7 @@ class HilbertMatrix(object):
             # for incrementing the matrix
             bedg_filename = "." + self.file + ".bedg"
             bedg = open(bedg_filename, 'w')
-            
+
             # use bedtools genomecov to create a BEDGRAPH
             # of the BAM file's coverage
             if self.chrom != "genome":
@@ -93,7 +91,7 @@ class HilbertMatrix(object):
                               % (self.file, self.chrom)
             else:
                 cmd = "bedtools genomecov -ibam %s -bg" % (self.file)
-            
+
             # save the BEDGRAPH to a file for the Hilbert Curve
             proc = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE)
             while True:
@@ -103,15 +101,15 @@ class HilbertMatrix(object):
                 else:
                     break
             bedg.close()
-            
+
             # it's BEDGRAPH, so use the 4th column for the matrix cells
             self.incr_column = 4
             self.temp_files.append(bedg_filename)
             return pbt.BedTool(bedg_filename)
-    
+
     def build(self):
         """
-        must compute a distance from the 
+        must compute a distance from the
         origing of the hilbert matrix start (0,0.
         to do so, we create a normalization factor which
         is the length of the chrom divided by the number
@@ -121,43 +119,44 @@ class HilbertMatrix(object):
         in the matrix using d2xy
         """
         # initialize the matrix
-        self.matrix = np.zeros((self.m_dim,self.m_dim), dtype=np.float)
+        self.matrix = np.zeros((self.m_dim, self.m_dim), dtype=np.float)
         ivls = self._get_intervals()
         for ivl in ivls:
             self.num_intervals += 1
             self.total_interval_length += ivl.end - ivl.start
-            
+
             start = ivl.start
             end = ivl.end
             if self.chrom == "genome":
                 offset = self.chrom_offsets[ivl.chrom]
                 start = ivl.start + offset
-                end   = ivl.end + offset
-                
+                end = ivl.end + offset
+
             # figure out what cell the start and end coords
             # of the interval belong in.
             # most of the time, the interval will fit in a single cell
             start_dist = int(start / self.norm_factor)
-            end_dist   = int(end / self.norm_factor)
-            
-            # however, we must populate EVERY cell that the 
+            end_dist = int(end / self.norm_factor)
+
+            # however, we must populate EVERY cell that the
             # interval spans.
             for dist in xrange(start_dist, end_dist + 1):
                 coords = d2xy(self.m_dim, dist)
                 if self.incr_column is None:
                     self._update_matrix(coords)
                 else:
-                    self._update_matrix(coords, \
+                    self._update_matrix(
+                        coords,
                         increment=float(ivl[self.incr_column - 1]))
         self._cleanup()
-        
-    def mask_low_values(self, min_val = 0):
+
+    def mask_low_values(self, min_val=0):
         rows, cols = self.matrix.shape
         for r in range(rows):
             for c in range(cols):
                 if self.matrix[r][c] <= min_val:
                     self.matrix[r][c] = np.NaN
-                    
+
     def norm_by_total_intervals(self):
         rows, cols = self.matrix.shape
         for r in range(rows):
